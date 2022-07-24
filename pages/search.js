24b9624e6d6b2ebfb30b100e6249fc6d583f8178
kevinsmasters <gmail@kevinmasters.com>
2022-07-23 21:56:30 -0400

    adds search filters

--- pages/search.js
+++ pages/search.js
@@ -7,7 +7,7 @@ import { BsFilter } from 'react-icons/bs';
 import Property from '../components/Property';
 import SearchFilters from '../components/SearchFilters';
 import noresult from '../assets/images/noresult.svg';
-
+import { fetchApi, baseUrl } from '../utils/fetchApi';
 const Search = ( { properties }) => {
     const [searchFilters, setSearchFilters] = useState(false);
     const router = useRouter();
@@ -48,13 +48,24 @@ const Search = ( { properties }) => {
 
 export default Search;
 
-export async function getStaticProps( { query }) {
+export async function getServerSideProps( { query }) {
     const purpose = query.purpose || 'for-rent';
     const rentFrequency = query.rentFrequency || 'yearly';
+  const minPrice = query.minPrice || '0';
+  const maxPrice = query.maxPrice || '1000000';
+  const roomsMin = query.roomsMin || '0';
+  const bathsMin = query.bathsMin || '0';
+  const sort = query.sort || 'price-desc';
+  const areaMax = query.areaMax || '35000';
+  const locationExternalIDs = query.locationExternalIDs || '5002';
+  const categoryExternalID = query.categoryExternalID || '4';
+
+  const data = await fetchApi(`${baseUrl}/properties/list?locationExternalIDs=${locationExternalIDs}&purpose=${purpose}&categoryExternalID=${categoryExternalID}&bathsMin=${bathsMin}&rentFrequency=${rentFrequency}&priceMin=${minPrice}&priceMax=${maxPrice}&roomsMin=${roomsMin}&sort=${sort}&areaMax=${areaMax}`);
+
     
     return {
       props: {
-        propertiesForSale: propertyForSale?.hits,
+        properties: data?.hits,
       }
     }
   }
